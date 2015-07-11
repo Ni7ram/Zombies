2f92927c56730f0ba68fbec9034e92769bcd0514
Ni7ram <nitram007@gmail.com>
2015-07-11 13:00:40 -0300

    asdasd

--- Pointer.cpp
+++ Pointer.cpp
@@ -10,6 +10,6 @@ void Pointer::draw(){
     Y = mouse_y;
     X = mouse_x;
 
-    circlefill(CANVAS, (int)X, (int)Y, 2, makecol(255,0,0));
+    circlefill(CANVAS, (int)X, (int)Y, 2, makecol(255,255,255));
     circle(CANVAS, (int)X, (int)Y, 5, makecol(255,0,0)); 
 };
